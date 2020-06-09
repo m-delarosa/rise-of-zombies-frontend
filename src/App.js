35c916ee6f0b2771a0287b21cd6a3439d8e157eb
github username <github email address>
2020-06-09 14:19:38 -0600

    Add skeletal JSX.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import FeaturedMovie from './components/FeaturedMovie'
+import Favorites from './components/Favorites'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>Rise of Zombies</h1>
+        <h4>Find a new zombie movie today.</h4>
+      </div>
+      <div className="Main-container">
+        <Favorites className="Favorites" />
+        <FeaturedMovie className="Movie-card" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function FeaturedMovie(props) {
+    return (
+        <div>
+            <h2>Movie Title</h2>
+            <h2>Movie Poster</h2>
+            <p>Release Date</p>
+            <p>Ratings</p>
+            <p>3.5/5</p>
+            <p>Number of Ratings</p>
+            <p>Overview</p>
+        </div>
+    )
+}
