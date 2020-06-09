0119dded0bd199c63f5ee63d10d51a4b3222b151
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-06-09 16:15:56 -0600

    Add Next Movie functionality.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import FeaturedMovie from './components/FeaturedMovie'
 import Favorites from './components/Favorites'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Rise of Zombies</h1>
-        <h4>Find a new zombie movie today.</h4>
+class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/movies')
+      .then(response => response.json())
+      .then(moviesList => this.setState({ movies: moviesList }))
+      .then(console.log("Movies Fetched!"))
+  }
+
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Rize</h1>
+          <h4>Raise the dead with a zombie movie today.</h4>
+        </div>
+        <div className="Main-container">
+          <Favorites className="Favorites" />
+          {this.state.movies.length > 0
+            ? <FeaturedMovie className="Movie-card" movie={this.state.movies[Math.floor(Math.random() * this.state.movies.length)]} />
+            : null
+          }
+        </div>
       </div>
-      <div className="Main-container">
-        <Favorites className="Favorites" />
-        <FeaturedMovie className="Movie-card" />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
